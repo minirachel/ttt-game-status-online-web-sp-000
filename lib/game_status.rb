fe4e87c41660ad881291b85b341cdd1e1cea6aec
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-14 21:56:04 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,40 @@
 # Helper Method
+require "pry"
+
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+#  !(board[index].nil? || board[index] == " ")
+  board[index] == "X" || board[index] == "O"
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [0,4,8], #left diagonals
+  [2,4,6], #right diagonals
+  [0,3,6], #left columns
+  [1,4,7], #middle column
+  [2,5,8] #right column
+]
+
+def won?(board)
+    WIN_COMBINATIONS.detect {|win_combination| board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && position_taken?(board, win_combination[0])}
+end
+
+
+
+
+###     WIN_COMBINATIONS.map {|win_combination| win_combination.find_all { |win_index| win_combination == [win_index[0], win_index[1], win_index[2]] } }
+
+###I'm missing how to check back into WIN_COMBINATIONS
+
+
+
+  #### if board(win_index).
+  ####  ^load value of the board position X or O
+
+#full?
+#draw?
+#over?
+#winner
