ff8889e9df8419de86bc6fc066bc98ada9e72d02
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-16 18:41:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,9 +38,7 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board)[0] == "X"
-    "X"
-  end
+  [won?(board)[0]]
   #this is referencing the index/integer but we want it to connect to the board's letter
   #input that argument into board to retrieve "X" "O" or nil
 
